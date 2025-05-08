4a6dcd16e17aa15b34a0d0de0448e1c085684344
Davut Bayık <80893519+davutbayik@users.noreply.github.com>
2025-05-08 21:21:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import os
 from autoWhatsApp import send_individual_contact, send_multiple_contacts
 import warnings
-warnings.filterwarnings("ignore")
-
-# Edit username for automatic logins
-PC_USER_NAME = "davutbayik"
+warnings.filterwarnings("ignore") # Silence warnings
 
 # Message configurations
 PHONE_NUMBER = "+905xxxxxxxxx" # Edit phone numbers
 PHONE_NUMBERS = ["+905xxxxxxxxx", "+905xxxxxxxxx"] # Edit phone numbers
 MESSAGE = "Hello, this is an automated message sent via autoWhatsApp module."
 HEADLESS_MODE = False
+
 ATTACHMENTS = os.listdir("assets")
 
 if __name__ == "__main__":
@@ -19,8 +17,7 @@ if __name__ == "__main__":
         phone_number=PHONE_NUMBER,
         message=MESSAGE,
         headless=HEADLESS_MODE,
-        pc_name=PC_USER_NAME,
-        attachments=ATTACHMENTS,
+        attachments=ATTACHMENTS
     )
 
     '''
@@ -28,7 +25,6 @@ if __name__ == "__main__":
         phone_numbers=PHONE_NUMBERS,
         message=MESSAGE,
         headless=HEADLESS_MODE,
-        pc_name=PC_USER_NAME,
         attachments=ATTACHMENTS
     )
     '''

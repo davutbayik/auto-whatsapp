a458cecbd503dfe251f43623ab51f38fb707f567
Davut Bayık <80893519+davutbayik@users.noreply.github.com>
2025-05-22 22:58:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import warnings
 warnings.filterwarnings("ignore") # Silence warnings
 
 # Message configurations
-PHONE_NUMBER = "+905343015153" # Edit phone numbers
+PHONE_NUMBER = "+905xxxxxxxxx" # Edit phone numbers
 PHONE_NUMBERS = ["+905xxxxxxxxx", "+905xxxxxxxxx"] # Edit phone numbers
 MESSAGE = "Hello, this is an automated message sent via autoWhatsApp module."
 HEADLESS_MODE = True
